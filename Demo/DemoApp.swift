847907d84df67d025222c76247870afecf83c964
AdiRam <adi.sr@outlook.com>
2025-06-06 11:16:44 -0500

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Ramaswamy, Aditya S on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DemoTests/DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTests.swift
+//  DemoTests
+//
+//  Created by Ramaswamy, Aditya S on 6/6/25.
+//
+
+import Testing
+@testable import Demo
+
+struct DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
